9c7c1bc24da45501da6473bf11c5c8bd6817f651
allenclement7 <allenclement7@gmail.com>
2024-08-19 14:44:24 -0400

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean("Miki", Parrot.class);
+
+        System.out.println(p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.print(n);
+    }
+}
---
+++ src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean("Koko")
+    Parrot parrot1(){
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean("Miki")
+    Parrot parrot2(){
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean("Riki")
+    Parrot parrot3(){
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+
+    @Bean
+    String hello(){
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten(){
+        return 10;
+    }
+}
