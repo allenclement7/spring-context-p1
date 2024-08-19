ee0d1f06b8ab2ef9b2c6c971594ef1e034efab38
allenclement7 <allenclement7@gmail.com>
2024-08-19 17:54:07 -0400

    Change of code after adding component annotation and componentscan

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,18 @@ package org.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
+        /*
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p);
 
         System.out.println(p.getName());
+        */
 
         /*
         String s = context.getBean(String.class);
@@ -20,5 +23,14 @@ public class Main {
         System.out.print(n);
 
          */
+
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> x;
+        context.registerBean("kiki", Parrot.class, parrotSupplier);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
     }
 }
